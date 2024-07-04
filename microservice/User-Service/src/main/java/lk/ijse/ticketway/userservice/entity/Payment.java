20fdb0fd400b0bec1e03d4bc48df3d815e48967b
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:27:11 +0530

    updete

--- microservice/User-Service/src/main/java/lk/ijse/ticketway/userservice/entity/Payment.java
+++ microservice/User-Service/src/main/java/lk/ijse/ticketway/userservice/entity/Payment.java
@@ -1,9 +1,37 @@
 package lk.ijse.ticketway.userservice.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String paymentId;
+    private String paymentType;
+    private Double paymentAmount;
+    private String paymentTime;
+    private String paymentStatus;
+    private String paymentLocation;
+    @CreationTimestamp
+    private Timestamp date;
+
+    @OneToOne
+    private Ticket ticket;
 }
