2ced59afb3745deab562fa5f4ef8b989fbc1fda5
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:16:09 +0530

    updete

--- microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/entity/Ticket.java
+++ microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/entity/Ticket.java
@@ -1,9 +1,41 @@
 package lk.ijse.ticketway.vehicleservice.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String ticketId;
+    private String ticketType;
+    private String ticketPrice;
+    private String ticketQty;
+    private String ticketDate;
+    private String ticketTime;
+    private String ticketStatus;
+    private String ticketLocation;
+
+    @ManyToOne
+    private User user;
+
+    @OneToOne(mappedBy = "ticket")
+    private Payment payment;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Vehicle vehicle;
 }
