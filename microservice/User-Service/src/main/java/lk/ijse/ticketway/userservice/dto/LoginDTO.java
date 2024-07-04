e37e1c4e0bb5a96c9857cdded49e0e3fe59ef3ca
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:27:47 +0530

    updete

--- microservice/User-Service/src/main/java/lk/ijse/ticketway/userservice/dto/LoginDTO.java
+++ microservice/User-Service/src/main/java/lk/ijse/ticketway/userservice/dto/LoginDTO.java
@@ -1,9 +1,18 @@
 package lk.ijse.ticketway.userservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDTO {
+    private String username;
+    private String password;
 }
