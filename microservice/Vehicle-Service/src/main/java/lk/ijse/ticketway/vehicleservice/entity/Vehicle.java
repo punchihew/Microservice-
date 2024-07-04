5e485ff6c911aac4844ba24a4255d15b57087b0a
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:15:29 +0530

    updete

--- microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/entity/Vehicle.java
+++ microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/entity/Vehicle.java
@@ -1,10 +1,14 @@
 package lk.ijse.ticketway.vehicleservice.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
 import java.util.List;
