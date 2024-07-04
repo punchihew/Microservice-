7fca0f5ab4d4fac2b603f8b61133f54933ce8347
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:14:18 +0530

    updete

--- microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/repository/VehicleRepository.java
+++ microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/repository/VehicleRepository.java
@@ -1,9 +1,15 @@
 package lk.ijse.ticketway.vehicleservice.repository;
 
+import lk.ijse.ticketway.vehicleservice.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
-public class VehicleRepository  {
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    Vehicle findByVehicleNumber(String vehicleNumber);
 }
