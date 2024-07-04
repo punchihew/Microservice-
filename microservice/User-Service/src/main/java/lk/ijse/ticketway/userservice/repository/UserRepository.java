1346f02ff9e4c303ae550846d9c4adee482c0141
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:25:51 +0530

    updete

--- microservice/User-Service/src/main/java/lk/ijse/ticketway/userservice/repository/UserRepository.java
+++ microservice/User-Service/src/main/java/lk/ijse/ticketway/userservice/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package lk.ijse.ticketway.userservice.repository;
 
+import lk.ijse.ticketway.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
-public interface UserRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
+    User findByEmail(String name);
 }
