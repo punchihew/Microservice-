dc3d078fff51659f24cda3cadcd09af5968c9cda
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:11:36 +0530

    updete

--- microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/service/VehicleService.java
+++ microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/service/VehicleService.java
@@ -1,9 +1,21 @@
 package lk.ijse.ticketway.vehicleservice.service;
 
+import lk.ijse.ticketway.vehicleservice.dto.VehicleDTO;
+import lk.ijse.ticketway.vehicleservice.util.ResponseDTO;
+
+import java.util.List;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
 public interface VehicleService {
+    ResponseDTO registerVehicle(VehicleDTO vehicleDTO);
+
+    ResponseDTO updateVehicle(VehicleDTO vehicleDTO);
+
+    List<VehicleDTO> getAllVehicles();
+
+    ResponseDTO getVehicle(String id);
 }
