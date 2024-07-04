546debffb5d621d5dbee8e34fe95eb3cf5cc54ad
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:10:58 +0530

    updete

--- microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/util/ResponseDTO.java
+++ microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/util/ResponseDTO.java
@@ -1,9 +1,22 @@
 package lk.ijse.ticketway.vehicleservice.util;
 
+import java.util.HashMap;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseDTO {
+    private int status;
+    private String message;
+    private HashMap<String, Object> data = new HashMap<>();
+
+    public ResponseDTO(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
 }
