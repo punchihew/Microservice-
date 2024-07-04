30fef851914dda314dc8a16d7e05047110ffe9cc
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:17:52 +0530

    updete

--- microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/config/WebAppConfig.java
+++ microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/config/WebAppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.ticketway.vehicleservice.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
