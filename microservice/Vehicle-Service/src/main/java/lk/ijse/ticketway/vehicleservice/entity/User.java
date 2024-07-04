019755843b5edf0733d0a0d70574321368d69987
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:15:49 +0530

    updete

--- microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/entity/User.java
+++ microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/entity/User.java
@@ -1,9 +1,34 @@
 package lk.ijse.ticketway.vehicleservice.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String contact;
+    private String address;
+    private String nic;
+
+    @OneToMany(mappedBy = "user")
+    private List<Ticket> tickets;
 }
