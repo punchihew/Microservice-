a76682c55cf79e6fd939ae828ea3f6589bc7fcbb
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:25:02 +0530

    updete

--- microservice/User-Service/src/main/java/lk/ijse/ticketway/userservice/util/ResponseDTO.java
+++ microservice/User-Service/src/main/java/lk/ijse/ticketway/userservice/util/ResponseDTO.java
@@ -1,9 +1,26 @@
 package lk.ijse.ticketway.userservice.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseDTO {
+    private int status;
+    private String message;
+    private HashMap<String, Object> data = new HashMap<>();
+
+    public ResponseDTO(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
 }
