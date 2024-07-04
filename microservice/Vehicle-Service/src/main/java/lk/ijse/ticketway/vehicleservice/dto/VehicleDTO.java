106540e6a329753728b543bb0a34e76c1e7fab39
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:17:01 +0530

    updete

--- microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/dto/VehicleDTO.java
+++ microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/dto/VehicleDTO.java
@@ -1,9 +1,32 @@
 package lk.ijse.ticketway.vehicleservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VehicleDTO {
+    private Long id;
+    private String vehicleNumber;
+    private String vehicleType;
+    private String vehicleBrand;
+    private String vehicleModel;
+    private String vehicleYear;
+    private String vehicleColor;
+    private String vehicleFuelType;
+    private String vehicleTransmissionType;
+    private String vehicleEngineCapacity;
+    private String vehicleMileage;
+    private String vehicleStatus;
+    private String vehicleLocation;
+    private Timestamp vehicleTime;
 }
