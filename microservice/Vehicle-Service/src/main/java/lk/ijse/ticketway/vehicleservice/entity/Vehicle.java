16e30eaae1e9141477d04cba2e5d924bf8857656
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:15:13 +0530

    updete

--- microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/entity/Vehicle.java
+++ microservice/Vehicle-Service/src/main/java/lk/ijse/ticketway/vehicleservice/entity/Vehicle.java
@@ -1,9 +1,43 @@
 package lk.ijse.ticketway.vehicleservice.entity;
 
+import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.List;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String vehicleNumber;
+    private String vehicleType;
+    private String vehicleBrand;
+    private String vehicleModel;
+    private String vehicleYear;
+    private String vehicleColor;
+    private String vehicleFuelType;
+    private String vehicleTransmissionType;
+    private String vehicleEngineCapacity;
+    private String vehicleMileage;
+    private String vehicleStatus;
+    private String vehicleLocation;
+    @CreationTimestamp
+    private Timestamp vehicleTime;
+
+    @OneToMany(mappedBy = "vehicle")
+    private List<Ticket> tickets;
 }
