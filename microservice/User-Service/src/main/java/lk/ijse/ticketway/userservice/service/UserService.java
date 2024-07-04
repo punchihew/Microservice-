c308cb07bd963da8cd2c06b92e80145d391bb266
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-04 20:25:15 +0530

    updete

--- microservice/User-Service/src/main/java/lk/ijse/ticketway/userservice/service/UserService.java
+++ microservice/User-Service/src/main/java/lk/ijse/ticketway/userservice/service/UserService.java
@@ -1,9 +1,19 @@
 package lk.ijse.ticketway.userservice.service;
 
+import lk.ijse.ticketway.userservice.dto.LoginDTO;
+import lk.ijse.ticketway.userservice.dto.UserDTO;
+
 /**
  * @version: v0.0.1
  * @author: Navishka
  * @date: 7/4/2024
  */
 public interface UserService {
+    void registerUser(UserDTO userDTO);
+
+    boolean loginUser(LoginDTO loginDTO);
+
+    void updateUser(UserDTO userDTO);
+
+    UserDTO getUser(String email);
 }
